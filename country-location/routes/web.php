c97d90e8185c7692a313b7dc9b9803814ef241e6
Vicky Chhetri <specialpower321@gmail.com>
2025-01-15 22:31:16 +0530

    web php

--- country-location/routes/web.php
+++ country-location/routes/web.php
@@ -4,10 +4,10 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/login', function () {
     return response()->json([
-        'status' => 'Error',
-        'message' => 'Something Went Wrong!',
+        'status' => 'error',
+        'message' => '401 Unauthorized Error !',
     ],404);
-});
+})->name('login');
 
 Route::get('/', function () {
     return view('welcome');
